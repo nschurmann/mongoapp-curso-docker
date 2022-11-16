225507adde05313c97301ce413bec522cd57e6c7
Luis José <luisdk.03@gmail.com>
2022-11-16 15:30:55 -0500

    change monguito to localhost in the mongo connection

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Animal = mongoose.model('Animal', new mongoose.Schema({
 
 const app = express()
 
-mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://nico:password@localhost:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
   console.log('listando... chanchitos...')
